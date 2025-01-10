54c09bf20ed8165c8ed6d712a889a93dde2304f8
jgoudiab <jeangabrielgoudiaby@gmail.com>
2025-01-10 21:01:42 +0000

    FIX: Simplification de la fonction display map

--- src/graphics/map.rs
+++ src/graphics/map.rs
@@ -2,18 +2,23 @@ use std::fs;
 
 use bevy::{
     app::{App, Plugin, Startup, Update},
-    asset::AssetServer,
+    asset::{AssetServer, Handle},
     math::Vec3,
     prelude::{
-        Commands, DespawnRecursiveExt, Entity, EventReader, IntoSystemConfigs, OnEnter, OnExit, Query, Res, Resource, Transform, With
+        Commands, DespawnRecursiveExt, Entity, EventReader, Image, IntoSystemConfigs, OnEnter,
+        OnExit, Query, Res, Resource, Transform, With,
     },
-    sprite::{Sprite, SpriteBundle}, window::{PrimaryWindow, Window, WindowResized},
+    sprite::{Sprite, SpriteBundle},
+    window::{PrimaryWindow, Window, WindowResized},
 };
 use serde::Deserialize;
 
 use crate::server::maze::{camera_controller, maze_setup, rotate_sky, CameraState, TreeParams};
 
-use super::{show_fps::{setup_fps_ui, update_fps_ui}, states::GameState};
+use super::{
+    show_fps::{setup_fps_ui, update_fps_ui},
+    states::GameState,
+};
 
 const TILE_SIZE: f32 = 15.0;
 const TEXTURE_SIZE: f32 = 114.0;
@@ -31,7 +36,7 @@ impl Plugin for MazePlugin {
         app.init_resource::<TreeParams>()
             .init_resource::<CameraState>()
             .init_resource::<MazeState>()
-            .add_systems(OnEnter(GameState::Game), maze_setup) 
+            .add_systems(OnEnter(GameState::Game), maze_setup)
             .add_systems(OnEnter(GameState::Game), display_minimap.after(maze_setup))
             .add_systems(Update, (camera_controller, rotate_sky).chain())
             .add_systems(OnExit(GameState::Game), cleanup_maze)
@@ -42,10 +47,11 @@ impl Plugin for MazePlugin {
 
 #[derive(Resource, Default)]
 pub struct MazeState {
-    pub is_ready: bool, 
+    pub is_ready: bool,
 }
+
 fn display_minimap(
-    mut commands: Commands,
+    commands: Commands,
     asset_server: Res<AssetServer>,
     windows: Query<&Window, With<PrimaryWindow>>,
     maze_state: Res<MazeState>,
@@ -54,28 +60,84 @@ fn display_minimap(
         return;
     }
 
-    let wall_texture = asset_server.load("white_maze.png");
-    let floor_texture = asset_server.load("black_maze.png");
+    let (wall_texture, floor_texture) = load_texture(asset_server);
+    let maze = read_map();
+    let (height, width) = calculate_maze_dimensions(&maze);
+    let (window_width, window_height) = calculate_window_dimensions(&windows);
+    let minimap_scale = 0.4;
+    let (minimap_width, minimap_height) =
+        calculate_minimap_dimensions(height, width, minimap_scale);
+    let (x_offset, y_offset) =
+        calculate_minimap_offsets(window_width, window_height, minimap_width, minimap_height);
 
-    let file_content = fs::read_to_string("src/utils/maps.json").expect("Unable to read file");
-    let maze: Maze = serde_json::from_str(&file_content).expect("Unable to parse JSON");
+    generate_minimap(
+        commands,
+        wall_texture,
+        floor_texture,
+        maze,
+        height,
+        width,
+        minimap_scale,
+        x_offset,
+        y_offset,
+    );
+}
 
-    let height = maze.maze_1.len();
-    let width = maze.maze_1[0].len();
+fn calculate_minimap_offsets(
+    window_width: f32,
+    window_height: f32,
+    minimap_width: f32,
+    minimap_height: f32,
+) -> (f32, f32) {
+    let x_offset = window_width / 2.0 - minimap_width - 20.0;
+    let y_offset = -window_height / 2.0 + minimap_height + 20.0;
+    (x_offset, y_offset)
+}
 
+fn calculate_minimap_dimensions(height: usize, width: usize, minimap_scale: f32) -> (f32, f32) {
+    let minimap_width = TILE_SIZE * width as f32 * minimap_scale;
+    let minimap_height = TILE_SIZE * height as f32 * minimap_scale;
+    (minimap_width, minimap_height)
+}
+
+fn calculate_window_dimensions(
+    windows: &Query<'_, '_, &Window, With<PrimaryWindow>>,
+) -> (f32, f32) {
     let window = windows.single();
     let window_width = window.width();
     let window_height = window.height();
+    (window_width, window_height)
+}
 
-    let minimap_scale = 0.4;
+fn calculate_maze_dimensions(maze: &Maze) -> (usize, usize) {
+    let height = maze.maze_1.len();
+    let width = maze.maze_1[0].len();
+    (height, width)
+}
 
-    let minimap_width = TILE_SIZE * width as f32 * minimap_scale;
-    let minimap_height = TILE_SIZE * height as f32 * minimap_scale;
+fn read_map() -> Maze {
+    let file_content = fs::read_to_string("src/utils/maps.json").expect("Unable to read file");
+    let maze: Maze = serde_json::from_str(&file_content).expect("Unable to parse JSON");
+    maze
+}
 
-    let x_offset = window_width / 2.0 - minimap_width - 20.0; 
-    let y_offset = -window_height / 2.0 + minimap_height + 20.0;
+fn load_texture(asset_server: Res<'_, AssetServer>) -> (Handle<Image>, Handle<Image>) {
+    let wall_texture = asset_server.load("white_maze.png");
+    let floor_texture = asset_server.load("black_maze.png");
+    (wall_texture, floor_texture)
+}
 
-    // Générer la minimap
+fn generate_minimap(
+    mut commands: Commands<'_, '_>,
+    wall_texture: Handle<Image>,
+    floor_texture: Handle<Image>,
+    maze: Maze,
+    height: usize,
+    width: usize,
+    minimap_scale: f32,
+    x_offset: f32,
+    y_offset: f32,
+) {
     for y in 0..height {
         for x in 0..width {
             let texture = if maze.maze_1[y][x] == 2 || maze.maze_1[y][x] == 0 {
@@ -109,11 +171,7 @@ pub fn update_minimap(
     sprites: Query<Entity, With<Sprite>>,
     maze_state: Res<MazeState>,
 ) {
-    if !maze_state.is_ready {
-        return; 
-    }
-
-    if resize_events.is_empty() {
+    if !maze_state.is_ready || resize_events.is_empty() {
         return;
     }
 
