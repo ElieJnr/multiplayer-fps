0d129a08701a6e0fa1b66ad66c10c21fc4cd7e91
babacandiaye <ndiaybabacarnjaay@gmail.com>
2025-01-21 15:27:13 +0000

    fix: refactoring

--- src/player/player.rs
+++ src/player/player.rs
@@ -171,7 +171,7 @@ pub fn create_players(commands: &mut Commands, player_animations: Res<PreloadedP
             SceneBundle {
                 scene: player_animations.model.clone(),
                 transform: Transform {
-                    translation: Vec3::new(pos[0], 0.0, pos[2]),
+                    translation: Vec3::new(pos[0], pos[1], pos[2]),
                     scale: Vec3::splat(0.25),
                     ..default()
                 },
