febb6f22f752fd7c89755233e981c2525c4cc744
babacandiaye <ndiaybabacarnjaay@gmail.com>
2025-01-24 21:12:20 +0000

    fix: synchronisation rotation

--- src/player/movement.rs
+++ src/player/movement.rs
@@ -1,16 +1,16 @@
-use crate::maze::models::*;
 use crate::common::protocol::{
     serialize_message, GameMessage, MessageContent, MessageType, NetworkConfig,
 };
 use crate::common::sync::NetworkMessages;
+use crate::maze::models::*;
 use crate::maze::models::{Collider, ColliderHouse, MazeState, ObstaclePositions};
 use crate::player::model::*;
 use bevy::input::mouse::MouseMotion;
 use bevy::input::ButtonInput;
 use bevy::math::{EulerRot, Quat, Vec2, Vec3};
 use bevy::prelude::{
-    AnimationPlayer, Camera3d, Commands, EventReader,
-    KeyCode, Local, Query, Res, ResMut, Transform, With, Without,
+    AnimationPlayer, Camera3d, Commands, EventReader, KeyCode, Local, Query, Res, ResMut,
+    Transform, With, Without,
 };
 use bevy::scene::SceneBundle;
 use bevy::time::Time;
@@ -33,6 +33,7 @@ pub fn player_movement(time: Res<Time>, keyboard_input: Res<ButtonInput<KeyCode>
         mouse_delta,
         ready: false,
     };
+
     if input.arrow_up || input.arrow_down || input.arrow_left || input.arrow_right || mouse_delta != Vec2::ZERO {
         *sequence_number += 1;
         let delta_time = time.delta_seconds();
@@ -41,30 +42,32 @@ pub fn player_movement(time: Res<Time>, keyboard_input: Res<ButtonInput<KeyCode>
             apply_input(&mut transform, &input, &movement, delta_time);
             if check_collisions(&transform, &collider_query, &house_collider_query) {
                 *transform = new_transform;
-            } else {
-                movement.position = transform.translation;
-                movement.rotation.y = transform.rotation.y;
-                let input_sequence = InputSequence {
-                    sequence_number: *sequence_number,
-                    timestamp: time.elapsed_seconds_f64(),
-                    input: input.clone(),
+            }
+            movement.position = transform.translation;
+            movement.rotation.y = transform.rotation.y;
+            let input_sequence = InputSequence {
+                sequence_number: *sequence_number,
+                timestamp: time.elapsed_seconds_f64(),
+                input: input.clone(),
+            };
+            movement.input_buffer.push_back(input_sequence);
+            if let Some(network) = network.as_ref() {
+                let message = GameMessage {
+                    message_type: MessageType::GameUpdate,
+                    sender: network.player_name.clone(),
+                    content: MessageContent::GameUpdate {
+                        position: (transform.translation.x, transform.translation.z),
+                        rotation: Vec2::new(
+                            transform.rotation.to_euler(EulerRot::XYZ).0,
+                            transform.rotation.to_euler(EulerRot::XYZ).1,
+                        ),
+                        sequence_number: *sequence_number,
+                        timestamp: time.elapsed_seconds_f64(),
+                        mouse_delta: input.mouse_delta,
+                    },
                 };
-                movement.input_buffer.push_back(input_sequence);
-                if let Some(network) = network.as_ref() {
-                    let message = GameMessage {
-                        message_type: MessageType::GameUpdate,
-                        sender: network.player_name.clone(),
-                        content: MessageContent::GameUpdate {
-                            position: (transform.translation.x, transform.translation.z),
-                            rotation: Vec2::new(transform.rotation.to_euler(EulerRot::XYZ).0, transform.rotation.to_euler(EulerRot::XYZ).1),
-                            sequence_number: *sequence_number,
-                            timestamp: time.elapsed_seconds_f64(),
-                            mouse_delta: input.mouse_delta,
-                        },
-                    };
-                    if let Some(msg_bytes) = serialize_message(&message) {
-                        let _ = network.client_socket.send(&msg_bytes);
-                    }
+                if let Some(msg_bytes) = serialize_message(&message) {
+                    let _ = network.client_socket.send(&msg_bytes);
                 }
             }
         }
@@ -184,16 +187,23 @@ pub fn manage_remote_players(mut commands: Commands, enemy_animations: Res<Prelo
                     transform.translation.x = *x;
                     transform.translation.z = *z;
                     transform.translation.y = 0.0;
-                    transform.rotate_y(-mouse_delta.x * 0.003);
+                    // if mouse_delta.length_squared() > 0.0 {
+                        transform.rotate_y(-mouse_delta.x * 0.003);
+                    // }
                 }
             } else {
                 let remote_player = commands
                     .spawn((
                         SceneBundle {
-                            scene: enemy_animations.model.clone(), 
+                            scene: enemy_animations.model.clone(),
                             transform: Transform {
                                 translation: Vec3::new(*x, 0.0, *z),
-                                rotation: Quat::from_euler(EulerRot::XYZ, 0.0, rotation.y + std::f32::consts::PI, 0.0),
+                                rotation: Quat::from_euler(
+                                    EulerRot::XYZ,
+                                    0.0,
+                                    rotation.y + std::f32::consts::PI,
+                                    0.0,
+                                ),
                                 scale: Vec3::splat(0.5),
                                 ..default()
                             },
@@ -203,7 +213,7 @@ pub fn manage_remote_players(mut commands: Commands, enemy_animations: Res<Prelo
                             name: player_name.clone(),
                         },
                         AnimationPlayer::default(),
-                        enemy_graph.graph.clone(), 
+                        enemy_graph.graph.clone(),
                         AnimationState::default(),
                     ))
                     .id();
