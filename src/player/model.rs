8621495778fa62560e6e8648a856115bace21d93
babacandiaye <ndiaybabacarnjaay@gmail.com>
2025-01-27 19:10:20 +0000

    fix: visualize shoot

--- src/player/model.rs
+++ src/player/model.rs
@@ -54,7 +54,7 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, preload_player_assets)
-            .add_systems(Update, (setup_player_animation, update_smoke_particles, add_smoke_trail,handle_keyboard_animation, update_bullets));
+            .add_systems(Update, (setup_player_animation, handle_keyboard_animation, update_bullets));
     }
 }
 
--- src/player/player.rs
+++ src/player/player.rs
@@ -5,68 +5,67 @@ use bevy::prelude::*;
 use std::{collections::HashMap, time::Duration};
 
 pub fn handle_keyboard_animation(keyboard: Res<ButtonInput<KeyCode>>, mut query: Query<(Entity, &mut AnimationState)>, animations: ResMut<PlayerAnimations>, mut animation_players: Query<&mut AnimationPlayer>, mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>, player_transform_query: Query<&Transform, With<PlayersComponent>>) {
-    if let Ok(player_transform) = player_transform_query.get_single() {
-        if let Ok((_entity, mut animation_state)) = query.get_single_mut() {
-            let new_animation = if keyboard.pressed(KeyCode::ArrowUp) {
-                "walk"
-            } else if keyboard.pressed(KeyCode::ArrowDown) {
-                "walk"
-            } else if keyboard.pressed(KeyCode::KeyR) {
-                "reload_fast"
-            } else if keyboard.pressed(KeyCode::Space) {
-                shoot_bullet(&mut commands, &mut meshes, &mut materials, player_transform);
-                "shoot"
-            } else if keyboard.just_released(KeyCode::ArrowUp) {
-                "stopwalk"
-            } else if keyboard.just_released(KeyCode::ArrowDown) {
-                "stopwalk"
-            } else if keyboard.just_released(KeyCode::Space) {
-                "stopshoot"
-            } else {
-                "static"
-            };
-    
-            if new_animation == "reload_fast" {
-                if new_animation != animation_state.current_animation {
-                    animation_state.current_animation = new_animation.to_string();
-    
-                    if let Some(player_entity) = animations.animation_player_entity {
-                        if let Ok(mut player) = animation_players.get_mut(player_entity) {
-                            if let Some(&animation_index) = animations.animations.get(new_animation) {
-                                let mut transitions = AnimationTransitions::new();
-                                transitions.play(
-                                    &mut player,
-                                    animation_index,
-                                    Duration::from_secs_f32(0.2),
-                                );
-                            }
-                        }
-                    }
-                }
-            } else if !new_animation.starts_with("stop") && new_animation != "reload_fast" {
-                if new_animation != animation_state.current_animation {
-                    animation_state.current_animation = new_animation.to_string();
-    
-                    if let Some(player_entity) = animations.animation_player_entity {
-                        if let Ok(mut player) = animation_players.get_mut(player_entity) {
-                            if let Some(&animation_index) = animations.animations.get(new_animation) {
-                                let mut transitions = AnimationTransitions::new();
-                                transitions
-                                    .play(&mut player, animation_index, Duration::from_secs_f32(0.2))
-                                    .repeat();
-                            }
-                        }
-                    }
-                }
-            } else if new_animation.starts_with("stop") {
+    if let Ok((_entity, mut animation_state)) = query.get_single_mut() {
+        let new_animation = if keyboard.pressed(KeyCode::ArrowUp) {
+            "walk"
+        } else if keyboard.pressed(KeyCode::ArrowDown) {
+            "walk"
+        } else if keyboard.pressed(KeyCode::KeyR) {
+            "reload_fast"
+        } else if keyboard.pressed(KeyCode::Space) {
+            let camera_transform = player_transform_query.get_single().unwrap();
+            shoot_bullet(&mut commands, &mut meshes, &mut materials, &camera_transform);
+            "shoot"
+        } else if keyboard.just_released(KeyCode::ArrowUp) {
+            "stopwalk"
+        } else if keyboard.just_released(KeyCode::ArrowDown) {
+            "stopwalk"
+        } else if keyboard.just_released(KeyCode::Space) {
+            "stopshoot"
+        } else {
+            "static"
+        };
+
+        if new_animation == "reload_fast" {
+            if new_animation != animation_state.current_animation {
+                animation_state.current_animation = new_animation.to_string();
+
                 if let Some(player_entity) = animations.animation_player_entity {
                     if let Ok(mut player) = animation_players.get_mut(player_entity) {
-                        if let Some(&run_animation) = animations
-                            .animations
-                            .get(new_animation.strip_prefix("stop").unwrap_or(""))
-                        {
-                            player.stop(run_animation);
+                        if let Some(&animation_index) = animations.animations.get(new_animation) {
+                            let mut transitions = AnimationTransitions::new();
+                            transitions.play(
+                                &mut player,
+                                animation_index,
+                                Duration::from_secs_f32(0.2),
+                            );
                         }
+                    }
+                }
+            }
+        } else if !new_animation.starts_with("stop") && new_animation != "reload_fast" {
+            if new_animation != animation_state.current_animation {
+                animation_state.current_animation = new_animation.to_string();
+
+                if let Some(player_entity) = animations.animation_player_entity {
+                    if let Ok(mut player) = animation_players.get_mut(player_entity) {
+                        if let Some(&animation_index) = animations.animations.get(new_animation) {
+                            let mut transitions = AnimationTransitions::new();
+                            transitions
+                                .play(&mut player, animation_index, Duration::from_secs_f32(0.2))
+                                .repeat();
+                        }
+                    }
+                }
+            }
+        } else if new_animation.starts_with("stop") {
+            if let Some(player_entity) = animations.animation_player_entity {
+                if let Ok(mut player) = animation_players.get_mut(player_entity) {
+                    if let Some(&run_animation) = animations
+                        .animations
+                        .get(new_animation.strip_prefix("stop").unwrap_or(""))
+                    {
+                        player.stop(run_animation);
                     }
                 }
             }
@@ -224,10 +223,10 @@ pub fn create_bullet(commands: &mut Commands, meshes: &mut ResMut<Assets<Mesh>>,
     });
 }
 
-pub fn shoot_bullet(commands: &mut Commands, meshes: &mut ResMut<Assets<Mesh>>, materials: &mut ResMut<Assets<StandardMaterial>>, player_transform: &Transform) {
+pub fn shoot_bullet(commands: &mut Commands, meshes: &mut ResMut<Assets<Mesh>>, materials: &mut ResMut<Assets<StandardMaterial>>, camera_transform: &Transform) {
     let bullet_mesh = meshes.add(Mesh::from(Cylinder {
-        radius: 0.003,
-        half_height: 0.1,
+        radius: 0.01,
+        half_height: 0.05,
         ..Default::default()
     }));
     let bullet_material = materials.add(StandardMaterial {
@@ -235,22 +234,24 @@ pub fn shoot_bullet(commands: &mut Commands, meshes: &mut ResMut<Assets<Mesh>>, 
         ..Default::default()
     });
 
-    let bullet_direction = player_transform.forward();
-
-    // let pos = get_window_dimensions(windows);
-
-    // let center = Vec3::new(pos.0/2., pos.1/2., 0.0);
+    let bullet_direction = camera_transform.forward();
+    let bullet_start_position =
+        camera_transform.translation + bullet_direction * 0.1 + Vec3::new(0.0, 0.1, 0.0);
 
     commands.spawn((
         PbrBundle {
             mesh: bullet_mesh,
             material: bullet_material,
-            transform: Transform::from_translation(player_transform.translation),
+            transform: Transform {
+                translation: bullet_start_position,
+                rotation: Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2),
+                ..Default::default()
+            },
             ..Default::default()
         },
         Bullet {
             direction: *bullet_direction,
-            speed: 100.0,
+            speed: 50.0,
         },
     ));
 }
