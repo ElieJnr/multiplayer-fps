2bc476386d39fe3ad68e2fcbe8a006673d0d0dd9
aliouniang <linosmart10@gmail.com>
2025-02-03 13:38:09 +0000

    .....

--- src/common/sync.rs
+++ src/common/sync.rs
@@ -59,16 +59,10 @@ fn check_network_messages(
             Some((data, _)) => {
                 if let Some(game_message) = deserialize_message(&data) {
                     match game_message.message_type {
-                        MessageType::DecreaseLife => {
-                            network_messages.0.push_back(game_message);
-                        }
-                        MessageType::GameUpdate => {
-                            network_messages.0.push_back(game_message);
-                        }
-                        MessageType::PlayerAction => {
-                            network_messages.0.push_back(game_message);
-                        }
-                        MessageType::SyncPlayers => {
+                        MessageType::DecreaseLife
+                        | MessageType::GameUpdate
+                        // | MessageType::PlayerAction
+                        | MessageType::SyncPlayers => {
                             network_messages.0.push_back(game_message);
                         }
                         _ => handle_game_message(game_message, &mut state),
